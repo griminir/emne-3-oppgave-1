03f77605c9e163ce9a7d24970ca2a0ba79741f80
griminir <viktor.elliot.king@gmail.com>
2024-04-25 11:14:17 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+var input = Console.ReadLine();
+Console.WriteLine($"Welcome {input}");
+Console.WriteLine($"So {input} what are you going to do today?");
+var input2  = Console.ReadLine();
+Console.WriteLine($"so mr.{input} is going to {input2}");
